f1162c0d3d9f32b021186495da05d631f8bd201c
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-10-28 22:40:05 +0300

    Add xox game codes with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,147 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { ReactNode, useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+type SquareType = "X" | "O" | null;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const calculateWinner = (squares: SquareType[]): SquareType => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+interface SquareProps {
+    value: SquareType
+    onClick(): void;
+}
+
+const Square: React.FC<SquareProps> = props => {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+interface BoardProps {
+    squares: SquareType[]
+    onClick(i: number): void;
+}
+
+const Board: React.FC<BoardProps> = props => {
+    const renderSquare = (i: number): ReactNode => {
+        return (
+            <Square
+                value={props.squares[i]}
+                onClick={() => props.onClick(i)}
+            />
+        );
+    };
+
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+const Game: React.FC = () => {
+    const [xIsNext, setXIsNext] = useState<boolean>(true);
+    const [stepNumber, setStepNumber] = useState<number>(0);
+    const [history, setHistory] = useState<{ squares: SquareType[] }[]>([
+        {
+            squares: Array(9).fill(null)
+        }
+    ]);
+
+
+    const handleClick = (i: number): void => {
+        const newHistory = history.slice(0, stepNumber + 1);
+        const current = newHistory[newHistory.length - 1];
+        const squares = current.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = xIsNext ? "X" : "O";
+        setHistory(newHistory.concat([
+            {
+                squares: squares
+            }
+        ]));
+        setStepNumber(newHistory.length);
+        setXIsNext(!xIsNext);
+    }
+
+    const jumpTo = (step: number): void => {
+        setStepNumber(step);
+        setXIsNext((step % 2) === 0);
+    }
+
+    const current = history[stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={i => handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
